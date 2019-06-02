0c58a586d7c88cc9030d77c4fcecdc081c063cfe
Garrett Swensen <romdame@gmail.com>
2019-06-02 16:03:38 -0600

    second commit

---
+++ src/Servers.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="index in 5">
+                        Server #{{ index }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+    </div>
+</template>
+
+
+
